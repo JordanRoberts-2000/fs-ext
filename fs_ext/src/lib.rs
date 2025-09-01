9646d6ceb9c34ef728c1a3a42391b0ff009acec5
Jordan Roberts <jordanroberts333@icloud.com>
2025-09-01 21:59:43 +0100

    feat: tokio helpers now behind a feature, can be optionally compiled

--- fs_ext/src/lib.rs
+++ fs_ext/src/lib.rs
@@ -5,10 +5,12 @@ mod error;
 pub mod formats;
 pub mod fsx;
 mod macros;
+#[cfg(feature = "tokio")]
 mod tokio_fsx;
 mod traits;
 pub(crate) mod utils;
 
+#[cfg(feature = "tokio")]
 pub mod tokio {
     pub mod fsx {
         pub use crate::tokio_fsx::*;
